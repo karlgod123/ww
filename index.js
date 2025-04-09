f0d2514f90ff14357bdbf4cc9fee474ca791b70f
programmelius <117346461+karlgod123@users.noreply.github.com>
2025-04-09 17:47:38 +0300

    hgf

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// Сделать так чтобы он не выходил со страницы а работал только на ней  также чтобы он создал в каждом производители папку парсинг и внутри папки ру, паспорт, фото, выписка
-// и отсылал каждую, также надо доделать код с циклом чтобы они по очереди проходили!!!приоритетно!
-
 const { expect } = require('@playwright/test');
 const { chromium } = require('playwright');
+const {listCodes} = require('./utils/listCodes');
+
 
 (async () => {
     const searchItems = [
@@ -27,35 +26,47 @@ const { chromium } = require('playwright');
             await page.fill('#id_q_mi_label_application', '');
             await page.fill('#id_q_mi_label_application', item);
             await page.press('#id_q_mi_label_application', 'Enter');
-            await page.waitForTimeout(3000);
+            await page.waitForTimeout(3020);
 
             // Проверка видимости элемента
             await expect(page.getByText(item)).toBeVisible();
-            await page.click('.data-fancybox');
+            await page.click('.data-fancybox'); 
+
+            // рандомахер
+            const randomTimeout = Math.floor(Math.random() * (5000 - 1000 + 1)) + 2000;
 
             // Скачивание РУ
             const downloadPromise = page.waitForEvent('download');
             await page.getByText('[Скачать РУ]').click();
             const download = await downloadPromise;
-            await download.saveAs(`/Парсер/РУ/${item}` + download.suggestedFilename());
-            await page.waitForTimeout(3000);
+
+            await download.saveAs(`//FileServer/Shara/Work/Прайсы/Прайсы индивидуальные/Сухих Никита Анатольевич/Парсер/РУ/${item + '-'}` + download.suggestedFilename());
+            await page.waitForTimeout(randomTimeout);
+
+            // Скачивание выписки
+            const extracts = page.waitForEvent('download');
+            await page.getByTitle('Скачать выписку').click();
+            const download_1 = await extracts;
+            await download_1.saveAs(`//FileServer/Shara/Work/Прайсы/Прайсы индивидуальные/Сухих Никита Анатольевич/Парсер/Выписки/${item}` + download.suggestedFilename());
+            await page.waitForTimeout(randomTimeout);
 
             // Скачивание инструкции
             const passport = page.waitForEvent('download');
             await page.getByText('[Скачать Инструкцию]').click();
             const download_2 = await passport;
-            await download_2.saveAs(`/Парсер/Инструкция/${item}` + download_2.suggestedFilename());
-            await page.waitForTimeout(3000);
+            await download_2.saveAs(`//FileServer/Shara/Work/Прайсы/Прайсы индивидуальные/Сухих Никита Анатольевич/Парсер/Паспорт/${item}` + download_2.suggestedFilename());
+            await page.waitForTimeout(randomTimeout);
 
             // Скачивание фото
             const flash = page.waitForEvent('download');
             await page.getByText('[Скачать Фото]').click();
             const download_3 = await flash;
-            await download_3.saveAs(`/Парсер/Фото/${item}` + download_3.suggestedFilename());
+            await download_3.saveAs(`//FileServer/Shara/Work/Прайсы/Прайсы индивидуальные/Сухих Никита Анатольевич/Парсер/Фото/${item}` + download_3.suggestedFilename());
+            await page.waitForTimeout(randomTimeout);
 
             // Возврат на страницу поиска для следующей итерации
             await page.goto('https://roszdravnadzor.gov.ru/services/misearch');
-            await page.waitForTimeout(2000);
+            await page.waitForTimeout(2030);
 
         } catch (error) {
             console.error(`Ошибка при обработке ${item}:`, error);
@@ -66,3 +77,7 @@ const { chromium } = require('playwright');
     // Закрытие браузера
     await browser.close();
 })();
+// Рандомайзер сделано
+// чтобы меня / на -
+// чтобы сохранял на сеть сделано
+// сохранял В разных производителей
